e59c2a5329a8112143eb0de1f0126a1d001b3e17
luis <lagonzalez1688@gmail.com>
2020-10-13 00:26:03 -0700

    Ejercicio variable 2

---
+++ src/ejerciciovariable2/Ejerciciovariable2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciovariable2;
+
+
+public class Ejerciciovariable2 {
+
+    
+    public static void main(String[] args) {
+        
+        int x=5;
+        int y=100;
+        double n=40.2;
+        double m=1.25;
+        
+        System.out.println("la suma de las variables es: "+(x+y));
+        System.out.println("la diferencia de las variables es: "+(x-y));
+        System.out.println("el producto de las variables es: "+(x*y));
+        System.out.println("el resto de las variables es: "+(x%y));
+        System.out.println("la suma de las variables es: "+(n+m));
+        System.out.println("la diferencia de las variables es: "+(n-m));
+        System.out.println("el producto de las variables es: "+(n*m));
+        System.out.println("el resto de las variables es: "+(n/m));
+        System.out.println("el resto de las variables es: "+(x/n));
+        System.out.println("el resto de las variables es: "+(y/m));
+        System.out.println("el resto de las variables es: "+(y%n));
+        System.out.println("el resto de las variables es: "+(x+y+n+m));
+        System.out.println("el resto de las variables es: "+(x*y*n*m));       
+    }
+    
+}
